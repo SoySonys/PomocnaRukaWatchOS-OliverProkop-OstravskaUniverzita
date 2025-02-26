4352918faa00318aeca68b5ea3e28a3a3ab55dc5
SoySonys <oliverprokop@icloud.com>
2025-02-26 22:10:02 +0100

    MainPage Initial setup + model creation

---
+++ PomocnaRuka Watch App/Model/MainPageState.swift
@@ -0,0 +1,8 @@
+//
+//  MainPageState.swift
+//  PomocnaRuka Watch App
+//
+//  Created by Oliver Prokop on 26.02.2025.
+//
+
+import Foundation
---
+++ PomocnaRuka Watch App/Views/MainPageCard.swift
@@ -0,0 +1,18 @@
+//
+//  MainPageCard.swift
+//  PomocnaRuka Watch App
+//
+//  Created by Oliver Prokop on 26.02.2025.
+//
+
+import SwiftUI
+
+struct MainPageCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainPageCard()
+}
