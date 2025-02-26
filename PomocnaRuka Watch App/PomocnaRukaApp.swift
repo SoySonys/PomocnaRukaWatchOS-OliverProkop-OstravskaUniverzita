16ddb3652161a448bf2dffadea869965807840d6
SoySonys <oliverprokop@icloud.com>
2025-02-26 19:04:53 +0100

    Initial Commit

---
+++ PomocnaRuka Watch App/PomocnaRukaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomocnaRukaApp.swift
+//  PomocnaRuka Watch App
+//
+//  Created by Oliver Prokop on 26.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct PomocnaRuka_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
