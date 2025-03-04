86ba9525090c02191a1e2bd7fe8e2afc4aca9c11
SoySonys <oliverprokop@icloud.com>
2025-03-04 20:11:31 +0100

    Resuscitacion page udpated. After resuscitacion page base done

---
+++ PomocnaRuka Watch App/Views/NotBreathing/AfterResuscitacion.swift
@@ -0,0 +1,18 @@
+//
+//  AfterResuscitacion.swift
+//  PomocnaRuka Watch App
+//
+//  Created by Oliver Prokop on 04.03.2025.
+//
+
+import SwiftUI
+
+struct AfterResuscitacion: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AfterResuscitacion()
+}
---
+++ PomocnaRuka Watch App/Views/Temp.swift
@@ -0,0 +1,18 @@
+//
+//  Temp.swift
+//  PomocnaRuka Watch App
+//
+//  Created by Oliver Prokop on 04.03.2025.
+//
+
+import SwiftUI
+
+struct Temp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Temp()
+}
