6a6a6886366b31e6786e5e9efa7febb7b4154b33
SoySonys <oliverprokop@icloud.com>
2025-03-04 16:49:15 +0100

    Resuscitacion base

---
+++ PomocnaRuka Watch App/Views/NotBreathing/Resuscitacion.swift
@@ -0,0 +1,18 @@
+//
+//  Resuscitacion.swift
+//  PomocnaRuka Watch App
+//
+//  Created by Oliver Prokop on 04.03.2025.
+//
+
+import SwiftUI
+
+struct Resuscitacion: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Resuscitacion()
+}
