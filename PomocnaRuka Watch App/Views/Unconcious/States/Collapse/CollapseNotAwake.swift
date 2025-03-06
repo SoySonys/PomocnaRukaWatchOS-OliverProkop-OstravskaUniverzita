4ffda1eeb38a3cd06171c6da1e7c6b4954b80647
SoySonys <oliverprokop@icloud.com>
2025-03-06 10:49:39 +0100

    Collapse state update

---
+++ PomocnaRuka Watch App/Views/Unconcious/States/Collapse/CollapseNotAwake.swift
@@ -0,0 +1,18 @@
+//
+//  CollapseNotAwake.swift
+//  PomocnaRuka Watch App
+//
+//  Created by Oliver Prokop on 05.03.2025.
+//
+
+import SwiftUI
+
+struct CollapseNotAwake: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CollapseNotAwake()
+}
