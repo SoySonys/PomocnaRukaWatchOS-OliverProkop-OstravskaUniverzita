d30ccb15457ae6fac1c19395f57944d496304c1a
SoySonys <oliverprokop@icloud.com>
2025-03-09 19:11:26 +0100

    WIP

---
+++ ComplicationWidget/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  ComplicationWidget
+//
+//  Created by Oliver Prokop on 09.03.2025.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource { "Configuration" }
+    static var description: IntentDescription { "This is an example widget." }
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
